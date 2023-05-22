dcf4b88dfd03dce7a49eed7b9e1383b75e0b8acd
renurawathokra <118500151+renurawathokra@users.noreply.github.com>
2023-05-22 22:46:14 +0530

    Create  Dep.java

---
+++ Dep.java
@@ -0,0 +1,12 @@
+ class Dep
+{ 
+ public static void main(String arr[]) 
+ { 
+ int a,b; 
+ for(a=1, b=5; a<=5; a++, b--) 
+ { 
+ System.out.print("a=" + a); 
+ System.out.println("\tb=" + b); 
+ } 
+ } 
+} 
