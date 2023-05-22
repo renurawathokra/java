ad565f753ef6d484d946092f7e95fe7744803757
renurawathokra <118500151+renurawathokra@users.noreply.github.com>
2023-05-22 22:35:21 +0530

    Create fristno.java

---
+++ fristno.java
@@ -0,0 +1,15 @@
+import java.util.scanner;
+class fristno
+{
+public static void main (string arr[])
+{
+int a,b,r;
+scaner ob=new scaner(system.in)
+system out.print("enter the frist no");
+a=ob next int();
+system out print("enter 2nd no);
+b=ob next int()
+r=a<b a=b;
+system.out println("largest"+r);
+}
+}
