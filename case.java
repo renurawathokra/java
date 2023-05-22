088bc07fc71efeced8daefd92850bdc4b84b3d15
renurawathokra <118500151+renurawathokra@users.noreply.github.com>
2023-05-22 22:31:20 +0530

    Create  case.java

---
+++ case.java
@@ -0,0 +1,48 @@
+# javaimport.java.util.scanner;
+class case
+
+{
+public static void main(String[] args)
+ {  
+         
+
+int class manth;
+char result;
+scanner ob=new scanner( System in)
+ 
+{
+System.out println("enter you class");
+class=ob nextint();
+System.out.println("enter a manth");
+manth=ob nextint();
+switch (ch)
+
+case 1;
+fese=manth*100;
+System.out.println("fese"+fese);
+break;
+
+case 2;
+fese=manth*200;
+System.out.println("fese"+fese);
+break;
+
+
+case 3;
+fese=manth*300;
+System.out.println("fese"+fese);
+break;
+
+
+case 4;
+fese=manth*400;
+System.out.println("fese"+fese);
+break;
+
+case 5;
+fese=manth*500;
+System.out.println("fese"+fese);
+break;
+
+ }
+   }
