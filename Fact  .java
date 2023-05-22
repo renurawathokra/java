44e83965f30ff1ae2507fedd8ed2f09eeac040bb
renurawathokra <118500151+renurawathokra@users.noreply.github.com>
2023-05-22 22:34:07 +0530

    Create Fact  .java

---
+++ Fact  .java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+ class Fact  
+{
+
+    public static void main(String[] args) {
+
+        int num = 10;
+        long factorial = 1;
+        for(int i = 1; i <= num; ++i)
+        {
+            
+            factorial *= i;
+        }
+        System.out.printf("Factorial of %d = %d", num, factorial);
+    }
+}S
