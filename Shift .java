a64adeb2b78869c230f3201767373f7174c13516
renurawathokra <118500151+renurawathokra@users.noreply.github.com>
2023-05-22 22:36:57 +0530

    Create  Shift .java

---
+++ Shift .java
@@ -0,0 +1,9 @@
+class Shift 
+{ 
+ public static void main(String arr[]) 
+ { 
+ int a=8; 
+ System.out.println("a="+a); 
+ System.out.println("a>>1="+(a>>1)); 
+ System.out.println("a<<1="+(a<<1)); 
+ }
