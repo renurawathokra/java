6cf21727f90a597d626f63979877952c1c6c033d
renurawathokra <118500151+renurawathokra@users.noreply.github.com>
2023-05-22 22:47:38 +0530

    Create Monu.java

---
+++ Monu.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Monu
+
+{
+    public static void main(String arr[])
+    {
+	int a,b,c,d;
+	scanner ob=new scanner(system in);
+	system.out println(enter a number");
+	a=ob nextint ();
+	
+     system.out println(enter 2nd number");
+	 b=ob next();
+	 system.out println(enter 3nd number");
+	 c=ob next();
+	 system.out println(enter 4nd number");
+	 d=ob next();
+	 if (a>b)
+	 {
+	 system.out println (a);
+	 }
+	 elseif
+	 {
+	 (b>c)
+	 {
+	 system.out println (b);
+	 }
+	 else 
+	 system.out println(d);
+	 }
+	 }
+	 }
+	 
+	 
