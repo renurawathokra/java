1791ecc813cfe509f52f355c81bd39f9e845474f
renurawathokra <118500151+renurawathokra@users.noreply.github.com>
2023-05-27 10:44:53 +0530

    Create roy.java

---
+++ roy.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Scanner;
+class roy
+{
+public static void main(String arr[])
+    {
+        int a,i=1;
+        Scanner ob=new Scanner(System.in);
+         System.out.println("enter no");
+        a=ob.nextInt();
+        while(i<=10)
+        {
+            System.out.println(a+""+i+"="+ai);
+            i++;
+        }
+        
+    }
+        }
