90ca6c6648dce8f228168dbd680992d06b83bddb
renurawathokra <118500151+renurawathokra@users.noreply.github.com>
2023-05-27 10:46:01 +0530

    Create  smallest .java

---
+++ smallest .java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class smallest 
+{
+public static void main(String arr[])
+ {
+System.out.println("Main");
+fun1();
+fun2();
+System.out.println("Main Again");
+ 
+ }
+static void fun1()
+ {
+
+int a,b;
+ Scanner ob = new Scanner(System.in);
+System.out.println("Enter Two no...");
+a = ob.nextInt();
+b = ob.nextInt();
+if(a>b)
+{
+System.out.println("smallest = " +b);
+ }
+ else
+{
+System.out.println("smallest = " +a);
+ }
+ }
+static void fun2()
+ {
+
+int a,b;
+Scanner ob = new Scanner(System.in);
+System.out.println("Enter Two no...");
+ a = ob.nextInt();
+ b = ob.nextInt();
+ if(a>b)
+ {
+ System.out.println("largest = " +a);
+ }
+else
+{
+ System.out.println("largest = " +b);
+}
+}
+}
