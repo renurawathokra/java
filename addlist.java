79bccb9aabbe46a5b6347725812fec01f328a582
renurawathokra <118500151+renurawathokra@users.noreply.github.com>
2023-05-31 21:53:38 +0530

    Create addlist.java

---
+++ addlist.java
@@ -0,0 +1,16 @@
+import java.util.*;
+ 
+public class Main
+{
+ 
+    public static void main(String args[])
+    {
+        LinkedList<String> ll = new LinkedList<>();
+ 
+        ll.add("Ram");
+        ll.add("shyam");
+        ll.add(1, "one");
+ 
+        System.out.println(ll);
+    }
+}
