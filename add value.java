cbf5b4c746a076d211737784266d514b53358a35
renurawathokra <118500151+renurawathokra@users.noreply.github.com>
2023-05-31 21:40:57 +0530

    Create add value.java

---
+++ add value.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Main {
+  
+    
+    public static void main(String args[])
+    {
+        
+        HashMap<Integer, String> hm1 = new HashMap<>();
+  
+        
+        HashMap<Integer, String> hm2
+            = new HashMap<Integer, String>();
+  
+        
+        hm1.put(1, "one");
+        hm1.put(2, "two");
+        hm1.put(3, "three");
+  
+        hm2.put(4, "four");
+        hm2.put(5, "five");
+        hm2.put(6, "six");
+  
+        
+        System.out.println("Mappings of HashMap hm1 are : "
+                           + hm1);
+  
+        System.out.println("Mapping of HashMap hm2 are : "
+                           + hm2);
+    }
+}
